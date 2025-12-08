5bfb818d1ca7b868ee14cf03fe6b4b8aa1524ae4
AHishamCG <127232185+AHishamCG@users.noreply.github.com>
2025-12-08 18:38:01 +0200

    Update DemoRestriction.php

--- app/Http/Middleware/DemoRestriction.php
+++ app/Http/Middleware/DemoRestriction.php
@@ -18,20 +18,6 @@ class DemoRestriction
      */
     public function handle(Request $request, Closure $next, ...$permissions)
     {
-        // dd(session()->all());
-        $user = Auth::user();
-        // dd($user->mobile);
-        if (config('constants.ALLOW_MODIFICATION') === 0) {
-
-            if ($user && $user->mobile === '9966338855') {
-                return $next($request);
-            }
-
-            if ($request->ajax() || $request->wantsJson()) {
-                return response()->json(['error' => true, 'error_message' =>  labels('admin_labels.this_operation_is_not_allowed_in_demo_mode', 'This operation is not allowed in demo mode.')]);
-            }
-            return redirect()->back()->with('error', labels('admin_labels.this_operation_is_not_allowed_in_demo_mode', 'This operation is not allowed in demo mode.'));
-        }
         return $next($request);
     }
 }
